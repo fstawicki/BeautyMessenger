46ee090054872ac6db05b7db4c846a26fc8dc171
fstawicki <83912962+fstawicki@users.noreply.github.com>
2022-05-15 00:09:40 +0200

    Add files via upload

---
+++ server/controllers/auth.js
@@ -0,0 +1,27 @@
+const { connect } = require('getstream');
+const bcrypt = require('bcrypt');
+const StreamChat = require('stream-chat');
+const crypto = require('crypto');
+
+const signup = (req, res) => {
+    try{
+        const {fullName, userName, password, phoneNumber} = req.body;
+        const userId = crypto;
+
+    }catch (error){
+        console.log(error);
+        res.status(500).json({ message: error });
+    }
+}
+
+
+const login = (req, res) => {
+    try{
+
+    }catch (error){
+        console.log(error);
+        res.status(500).json({ message: error });
+    }
+}
+
+module.exports = {signup, login};
--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 
-const app = express();
+const authRoutes = require("./routes/auth.js");
 
+const app = express();
 const PORT = process.env.PORT || 5000;
 
 require('dotenv').config();
@@ -10,3 +11,13 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
+
+app.get('/', (req,res) => {
+    res.send('Hello, world');
+});
+
+app.use('/auth', authRoutes);
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+});
